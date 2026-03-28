96b45b76d9ffd5f7aa69aeb5649e8109f62c78a1
Holt <holt007ab@gmail.com>
2026-03-28 19:32:39 +0100

    añadido un nuevo grupo de dependencias para las librerias usadas en testing. añadido pytest como framework de testing
    añadido en el fichero de test los 5 test que se completaran ahora

--- tests/test_xsdata_runner_unit.py
+++ tests/test_xsdata_runner_unit.py
@@ -1,1 +1,22 @@
-import 
+import pytest as pt
+
+from cvn_codegen.xsdata_runner import (
+    XSDTargetSpec,
+    RunnerError,
+)
+
+def test_resolve_single_target_returns_expected_spec():
+    
+    pass
+
+def test_resolve_all_returns_targets_in_stable_order():
+    pass
+
+def test_build_xsdata_command_for_cnv_uses_expected_arguments():
+    pass
+
+def test_invalid_target_raises_runner_error():
+    pass
+
+def test_smoke_test_model_creates_python_files():
+    pass
